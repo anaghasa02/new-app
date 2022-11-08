92942f79c9b2e5c35f118f3ae68ae1c4485518b4
Anagha <sa.anagha15@gmail.com>
2022-11-08 17:41:43 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import{Route,Link} from "react-router-dom";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Route exact path="/home"component={Home} />
+      <Route exact path="/about"component={About} />
+      <Route exact path="/projects"component={Projects} />
+      <Route exact path="/contact"component={Contact} />
+    </div>
+  );
+}
+
+export default App;
